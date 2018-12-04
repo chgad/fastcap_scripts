66dcbe78f742db41d7bdebf66a21105fcd1e553b
chgad <chgad.games@gmail.com>
2018-12-04 11:34:21 +0100

    working idt upper and lower structures

--- structures.py
+++ structures.py
@@ -1,8 +1,9 @@
 # Ein Skript um Listfiles für FastCap zu erstellen
 
 
-import numpy as np
 import copy as cp
+
+import numpy as np
 
 
 class GeomFace:
@@ -83,7 +84,6 @@ class GeomFace:
 
         return base_string.format(*self.corners.flatten(), shape=shape_indicator, name=conductor_name)
 
-
 # Test for Geometric faces
 # g = GeomFace(3, np.array([[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 0.0, 0.0]]))
 
@@ -94,11 +94,41 @@ class GeomFace:
 # print(p.prep_export_string())
 
 
-class ElectrodeStructure:
+class GeomFaceList:
+    """
+    A Container for multiple Faces.
+
+    """
+    def __init__(self, faces):
+        self.faces = faces
+
+        self.validate()
+
+    def validate(self):
+        if not isinstance(self.faces, list):
+            raise ValueError("The profided Value for 'faces' is not a list.")
+
+        for face in self.faces:
+            if not isinstance(face, GeomFace):
+                raise ValueError("The values of 'faces' aren't GeomFace instances.")
+
+    def append(self, face):
+        if not isinstance(face, GeomFace):
+            raise ValueError("The provided face is not a GeomFace instances.")
+        self.faces.append(face)
+
+    def prep_export_string(self):
+        export_string = ""
+        for face in self.faces:
+            export_string += face.prep_export_string()
+        return export_string
+
+
+class Cuboid:
     def __init__(self, length, width, height):
         self.length = length
         self.width = width
-        self.heigth = height
+        self.height = height
 
         self.left_face = None
         self.right_face = None
@@ -108,6 +138,7 @@ class ElectrodeStructure:
         self.top_face = None
 
         self.setup_all_faces()
+        self.all_faces = self.get_all_faces()
 
     def validate(self):
         if not self.length > 0.0:
@@ -116,14 +147,14 @@ class ElectrodeStructure:
         if not self.width > 0.0:
             raise ValueError("width must be non-negative and greater than 0.")
 
-        if not self.heigth > 0.0:
+        if not self.height > 0.0:
             raise ValueError("height must be non-negative and greater than 0.")
 
     def set_left_face(self):
         zero = np.array([0.0, 0.0, 0.0])
         one = np.array([0.0, self.length, 0.0])
-        two = np.array([0.0, self.length, self.heigth])
-        three = np.array([0.0, 0.0, self.heigth])
+        two = np.array([0.0, self.length, self.height])
+        three = np.array([0.0, 0.0, self.height])
         self.left_face = GeomFace(4, np.array([zero, one, two, three]))
 
     def set_right_face(self):
@@ -131,8 +162,8 @@ class ElectrodeStructure:
 
     def set_front_face(self):
         zero = np.array([0.0, 0.0, 0.0])
-        one = np.array([0.0, 0.0, self.heigth])
-        two = np.array([self.width, 0.0, self.heigth])
+        one = np.array([0.0, 0.0, self.height])
+        two = np.array([self.width, 0.0, self.height])
         three = np.array([self.width, 0.0, 0.0])
         self.front_face = GeomFace(4, np.array([zero, one, two, three]))
 
@@ -147,7 +178,7 @@ class ElectrodeStructure:
         self.bottom_face = GeomFace(4, np.array([zero, one, two, three]))
 
     def set_top_face(self):
-        self.top_face = self.bottom_face.copy() + np.array([0.0, 0.0, self.heigth])
+        self.top_face = self.bottom_face.copy() + np.array([0.0, 0.0, self.height])
 
     def setup_all_faces(self):
         self.set_front_face()
@@ -157,9 +188,38 @@ class ElectrodeStructure:
         self.set_bottom_face()
         self.set_top_face()
 
+    def get_all_faces(self):
+        return [getattr(self, face) for face in self.__dict__ if face.endswith("face") and getattr(self, face)]
+
+    def copy(self):
+        return cp.deepcopy(self)
+
+    def __add__(self, other):
+        if not isinstance(other, np.ndarray):
+            raise ValueError("__add__ is a translation operation. A Face can only be added with a 3D vectore")
+        for face in self.all_faces:
+            face += other
+
+        return self
+
+    def __sub__(self, other):
+        if not isinstance(other, np.ndarray):
+            raise ValueError("__sub__ is a translation operation. A Face can only be added with a 3D vectore")
+        for face in self.all_faces:
+            face -= other
+
+        return self
+
+
+class ElectrodeStructure(Cuboid):
+    def __init__(self, omit_faces=[], *args, **kwargs):
+        super(ElectrodeStructure, self).__init__(*args, **kwargs)
+        self.omit_faces = omit_faces
+
     def prep_export_string(self,cond_name=1):
         export_string = ""
-        for face in [getattr(self, x) for x in self.__dict__ if x.endswith("face") and getattr(self, x)]:
+        faces_to_export = [face for face in self.all_faces if not face in self.omit_faces]
+        for face in faces_to_export:
             export_string += face.prep_export_string()
 
         return export_string
@@ -173,11 +233,10 @@ class ElectrodeStructure:
 # Test the Electrode Structure
 
 
-elec = ElectrodeStructure(length=7.0, width=1.5, height=3.7)
-print(elec.prep_export_string())
-elec.export_to_file("fast_cap_test.txt")
+# elec = ElectrodeStructure(length=7.0, width=1.5, height=3.7) #omit_faces=["bottom_face"])
+# print(elec.prep_export_string())
+# elec.export_to_file("fast_cap_test2.txt")
 
-#
 # print("Bottom and top Face")
 # print(elec.bottom_face)
 # print(elec.top_face)
@@ -188,6 +247,208 @@ elec.export_to_file("fast_cap_test.txt")
 # print(elec.left_face)
 # print(elec.right_face)
 
-class BaseStructure:
-    def __init__(self):
-        pass
+
+class LowerBaseStructure(Cuboid):
+
+    """
+    The BaseStructure will look like the followning scheme:
+
+                    back
+
+    1eee2---3eee4---5eee6---7eee8---9eee10
+    |                                   |
+    |                                   |
+    0-----------------------------------11
+                    front
+
+    in the x-y-plane.
+
+    The marker eee show where no face is defined and an ElectrodeStructure is added
+    later on.
+
+
+    """
+    def __init__(self, elec_width, elec_sep, elec_cnt, omit_faces=[],*args, **kwargs):
+
+        self.elec_width = elec_width    # elec distance in y-direction
+        self.elec_sep = elec_sep        # distance between 2 Electrodes
+        self.elec_cnt = elec_cnt        # number of electrodes
+
+        super(LowerBaseStructure, self).__init__(*args, **kwargs)
+
+        self.omit_faces = omit_faces
+
+    def set_back_face(self):
+        zero = np.array([0.0, 0.0, 0.0])
+        one = np.array([0.0, 0.0, self.height])
+        two = np.array([2*self.elec_sep + self.elec_width, 0.0, self.height])
+        three = np.array([2*self.elec_sep + self.elec_width, 0.0, 0.0])
+        base_face = GeomFace(4, np.array([zero, one, two, three]))
+        separation = 2 * (self.elec_width + self.elec_sep)
+        faces_list = [base_face.copy() + np.array([n * separation + self.elec_width, self.length, 0.0])
+                      for n in range(self.elec_cnt-1)]
+
+        self.back_face = GeomFaceList(faces=faces_list)
+
+    def prep_export_string(self,cond_name=1):
+        export_string = ""
+        faces_to_export = [face for face in self.all_faces if not face in self.omit_faces]
+        for face in faces_to_export:
+            export_string += face.prep_export_string()
+
+        return export_string
+
+
+class UpperBaseStructure(Cuboid):
+    """
+    The BaseStructure will look like the followning scheme:
+
+                    back
+
+    1-------------------------------------------2
+    |                                           |
+    |                                           |
+    0----+eee*-----9eee8-----7eee6-----5eee4----3
+                    front
+
+    in the x-y-plane.
+
+    The marker eee show where no face is defined and an ElectrodeStructure is added
+    later on.
+
+
+    """
+    def __init__(self, elec_width, elec_sep, elec_cnt, omit_faces=[],*args, **kwargs):
+
+        self.elec_width = elec_width    # elec distance in y-direction
+        self.elec_sep = elec_sep        # distance between 2 Electrodes
+        self.elec_cnt = elec_cnt        # number of electrodes
+
+        super(UpperBaseStructure, self).__init__(*args, **kwargs)
+
+        self.omit_faces = omit_faces
+
+    def set_front_face(self):
+        zero = np.array([0.0, 0.0, 0.0])
+        one = np.array([0.0, 0.0, self.height])
+        two = np.array([2 * self.elec_sep + self.elec_width, 0.0, self.height])
+        three = np.array([2 * self.elec_sep + self.elec_width, 0.0, 0.0])
+        sec_three = np.array([self.elec_sep + self.elec_width, 0.0, 0.0])
+
+        start_end_face = base_face = GeomFace(4, np.array([zero, one, two, sec_three]))
+        base_face = base_face = GeomFace(4, np.array([zero, one, two, three]))
+
+        separation = 2 * (self.elec_width + self.elec_sep)
+        faces_list = [base_face.copy() + np.array([n * separation + 2 * self.elec_width + self.elec_sep, 0.0, 0.0])
+                      for n in range(self.elec_cnt - 1)]
+        faces_list += [start_end_face,
+                       start_end_face.copy() + np.array([self.width - (self.elec_sep + self.elec_width), 0.0, 0.0])]
+
+    def set_back_face(self):
+        zero = np.array([0.0, 0.0, 0.0])
+        one = np.array([0.0, 0.0, self.height])
+        two = np.array([self.width, 0.0, self.height])
+        three = np.array([self.width, 0.0, 0.0])
+        self.back_face = GeomFace(4, np.array([zero, one, two, three])) + np.array([0.0, self.length, 0.0])
+
+    def prep_export_string(self,cond_name=1):
+        export_string = ""
+        faces_to_export = [face for face in self.all_faces if not face in self.omit_faces]
+        for face in faces_to_export:
+            export_string += face.prep_export_string()
+
+        return export_string
+
+
+class IdtLowerStructure:
+
+    def __init__(self, elec_length, elec_width , elec_sep, elec_cnt,
+                 base_length, height):
+
+        self.elec_length = elec_length
+        self.elec_width = elec_width
+        self.elec_sep = elec_sep
+        self.elec_cnt = elec_cnt
+
+        self.base_length = base_length
+        self.base_width = self.set_base_width()
+
+        self.height = height
+
+        self.electrodes = None
+        self.base = None
+
+        self.setup()
+
+    def setup(self):
+        self.set_base()
+        self.set_electrodes()
+
+    def set_base_width(self):
+        return self.elec_cnt * self.elec_width + (self.elec_cnt-1)*(self.elec_width+2*self.elec_sep)
+
+    def set_base(self):
+        self.base = LowerBaseStructure(elec_width=self.elec_width, elec_sep=self.elec_sep, elec_cnt=self.elec_cnt,
+                                       length=self.base_length, width=self.base_width, height=self.height,
+                                       omit_faces=["bottom_face"])
+
+    def set_electrodes(self):
+        electrode = ElectrodeStructure(length=self.elec_length, width=self.elec_width, height=self.height,
+                                       omit_faces=["bottom_face", "front_face"])
+        separation = 2*(self.elec_width + self.elec_sep)
+        self.electrodes = [electrode.copy() + np.array([n * separation, self.base_length, 0.0])
+                           for n in range(self.elec_cnt)]
+
+    def prep_export_strings(self):
+
+        export_cond_string = ""
+        export_diel_string = ""
+
+        export_cond_string += self.base.prep_export_string()
+        export_diel_string += self.base.bottom_face.prep_export_string()
+        for elec in self.electrodes:
+            export_cond_string += elec.prep_export_string()
+            export_diel_string += elec.bottom_face.prep_export_string()
+
+        return export_cond_string, export_diel_string
+
+    def export_to_file(self,cond_file_name, diel_file_name):
+        cond, diel = self.prep_export_strings()
+        with open(cond_file_name, "w") as f:
+            f.write(cond)
+            f.close()
+
+        with open(diel_file_name, "w")as f:
+            f.write(diel)
+            f.close()
+
+
+# Test for IdtLowerStructure
+# idt_lower = IdtLowerStructure(elec_length=20, elec_width=2, elec_sep=3, elec_cnt=10, base_length=5, height=7)
+#
+# idt_lower.export_to_file(cond_file_name="test_cond_file.txt", diel_file_name="test_diel_file.txt")
+
+class IdtUpperStructure(IdtLowerStructure):
+
+    def set_base_width(self):
+        # originally self.elec_cnt * self.elec_width + (self.elec_cnt-1)*(self.elec_width+2*self.elec_sep)
+        # + 2*(self.elec_width + self.elec_sep
+        return (self.elec_cnt+1) * self.elec_width + self.elec_cnt*(self.elec_width+2*self.elec_sep)
+
+    def set_base(self):
+        self.base = UpperBaseStructure(elec_width=self.elec_width, elec_sep=self.elec_sep, elec_cnt=self.elec_cnt,
+                                       length=self.base_length, width=self.base_width, height=self.height,
+                                       omit_faces=["bottom_face"])
+
+    def set_electrodes(self):
+        electrode = ElectrodeStructure(length=self.elec_length, width=self.elec_width, height=self.height,
+                                       omit_faces=["bottom_face", "back_face"])
+        separation = 2 * (self.elec_width + self.elec_sep)
+        self.electrodes = [electrode.copy() + np.array([(n+0.5) * separation, -self.elec_length, 0.0])
+                           for n in range(self.elec_cnt)]
+
+
+# Test for IdtLowerStructure
+idt_upper = IdtUpperStructure(elec_length=20, elec_width=2, elec_sep=3, elec_cnt=10, base_length=5, height=7)
+
+idt_upper.export_to_file(cond_file_name="test_upper_cond_file.txt", diel_file_name="test_upper_diel_file.txt")
