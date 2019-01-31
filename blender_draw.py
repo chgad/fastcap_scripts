4a1890d9ae34e64bc5c1eebb2f7b2d291b050824
chgad <chgad.games@gmail.com>
2019-01-31 18:38:25 +0100

    worked on belnder_draw until some satisfying output is created

--- blender_draw.py
+++ blender_draw.py
@@ -2,6 +2,7 @@ from faces import GeomFace, GeomFaceList
 from structures import Cuboid, LowerBaseStructure, UpperBaseStructure, IdtLowerStructure, IdtUpperStructure, SiO2Layer
 import numpy as np
 import bpy
+import time
 
 width, length, height = 1.0, 1.0, 1.0
 
@@ -25,6 +26,22 @@ width, length, height = 1.0, 1.0, 1.0
 #
 # vertices, faces = cube.prep_blender_data()
 
+
+def list_electrode_prep_blender_data(cuboid_list, face_name):
+
+    exp_vertices =[]
+    exp_faces = []
+    start_index = 0
+    for cuboid in cuboid_list:
+        face_obj = getattr(cuboid, face_name)
+        vertices, face = face_obj.prep_blender_data(start_index)
+        exp_vertices.extend(vertices)
+        exp_faces.append(face)
+        start_index += 4
+
+    return exp_vertices, exp_faces
+
+
 elec_width = 1
 elec_sep = 2
 elec_cnt = 2
@@ -32,6 +49,13 @@ length *= 3
 elec_length = 5
 width = elec_cnt * elec_width + (elec_cnt-1)*(elec_width+2*elec_sep)
 height *= 1
+
+modify = True
+export = True
+subdivide_all = True
+insets = 2
+subdivides = 2
+title = "0  NO_SUB_DIVS\n"
 
 # base_lower = LowerBaseStructure(elec_width=elec_width, elec_sep=elec_sep, elec_cnt=elec_cnt,
 #                                 length=length, width=width, height=height,
@@ -69,6 +93,18 @@ idt_upper + align_idt_vector
 
 si_o2 - np.array([0.0, 0.0, 6.0])
 
+# To be simplified after succesful simulation
+
+lower_elec_data = list_electrode_prep_blender_data(idt_lower.electrodes, "bottom_face")
+
+upper_elec_data = list_electrode_prep_blender_data(idt_upper.electrodes, "bottom_face")
+
+lower_base_bottom_verts, lower_face = idt_lower.base.bottom_face.prep_blender_data()
+upper_base_bottom_vaerts, upper_face = idt_upper.base.bottom_face.prep_blender_data()
+
+upper_base_bottom_data = (upper_base_bottom_vaerts, (upper_face,))
+lower_base_bottom_data = (lower_base_bottom_verts, (lower_face,))
+
 upper_data = idt_upper.prep_blender_data()
 
 lower_data = idt_lower.prep_blender_data()
@@ -83,21 +119,54 @@ sio2_data = si_o2.prep_blender_data()
 objects = {
     "IDT_UPPER": upper_data,
     "IDT_LOWER": lower_data,
+    "BASE_BOTTOM_UPPER": upper_base_bottom_data,
+    "BASE_BOTTOM_LOWER": lower_base_bottom_data,
+    "ELEC_UPPER": upper_elec_data,
+    "ELEC_LOWER": lower_elec_data,
     "DIEL_UPPER": diel_upper_data,
     "DIEL_LOWER": diel_lower_data,
     "SI_O2": sio2_data
 }
 
-mat_one = bpy.data.materials.new(name="Conductor")
-mat_one.diffuse_color = (0.0, 0.0, 1.0)
-mat_two = bpy.data.materials.new(name="Dielectric")
-mat_two.diffuse_color = (0.0, 1.0 ,0.0)
+# Material - file_name Dictionary
+
+mat_file_name_dict = {
+    "Conductor Air Upper": "idt_upper_cond_air.txt",
+    "Conductor Air Lower": "idt_lower_cond_air.txt",
+    "Dielectric": "dielectric.txt",
+    "Conductor Dielectric Upper": "idt_upper_cond_diel.txt",
+    "Conductor Dielectric Lower": "idt_lower_cond_diel.txt"
+}
+
+mat_one = bpy.data.materials.new(name="Conductor Air Upper")
+mat_one.diffuse_color = (0.0, 0.0, 1.0)  # Blue
+mat_two = bpy.data.materials.new(name="Conductor Air Lower")
+mat_two.diffuse_color = (0.0, 0.5, 0.5)
+
+mat_three = bpy.data.materials.new(name="Conductor Dielectric Upper")
+mat_three.diffuse_color = (1.0, 0.0, 0.0)  # Red
+mat_four = bpy.data.materials.new(name="Conductor Dielectric Lower")
+mat_four.diffuse_color = (0.5, 0.5, 0.0)
+
+mat_five = bpy.data.materials.new(name="Dielectric")
+mat_five.diffuse_color = (0.0, 1.0, 0.0)  # Green
 
 for name, tpl in objects.items():
     mat = mat_two
 
     if name.startswith("IDT"):
-        mat = mat_one
+        if name.endswith("UPPER"):
+            mat = mat_one
+        else:
+            mat = mat_two
+
+    elif name.startswith("ELEC") or name.startswith("BASE_BOTTOM"):
+        if name.endswith("UPPER"):
+            mat = mat_three
+        else:
+            mat = mat_four
+    else:
+        mat = mat_five
 
     mesh_data = bpy.data.meshes.new(name)
     mesh_data.from_pydata(tpl[0], [], tpl[1])
@@ -106,8 +175,66 @@ for name, tpl in objects.items():
     obj.data.materials.append(mat)
     bpy.context.scene.objects.link(obj)
 
+    bpy.context.scene.objects.active = obj
 
 
+
+if modify:
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.join()
+
+    bpy.ops.object.mode_set(mode="EDIT")
+
+
+    for i in range(insets):
+        bpy.ops.mesh.inset()
+
+    # only the las insets get divided
+
+    bpy.ops.mesh.select_all(action="SELECT")
+    bpy.ops.mesh.remove_doubles()
+    if not subdivide_all:
+        bpy.ops.mesh.select_all(action="DESELECT")
+    if subdivides:
+        bpy.ops.mesh.subdivide(number_cuts=subdivides)
+
+    bpy.ops.mesh.separate(type="MATERIAL")
+    bpy.ops.object.mode_set(mode="OBJECT")
+
+bpy.ops.object.select_all(action="DESELECT")
+
+# obj = bpy.context.scene.objects[0].data
+#
+# print(bpy.context.scene.objects[0].data.materials[0].name)
+
+# This should become a function
+# print([(x.name, list(x.data.materials), len(x.data.polygons)) for x in bpy.context.scene.objects])
+if export:
+    for obj in bpy.context.scene.objects:
+        data = obj.data
+        exp_str = title
+        for face in data.polygons:
+            face_verts = face.vertices
+
+            base_str = "Q  1"
+            if len(face_verts) == 3:
+                base_str = "T  1"
+            exp_str += base_str
+            for i in face_verts:
+                vert = data.vertices[i].co
+                exp_str += "  {} {} {}".format(vert.x, vert.y, vert.z)
+            exp_str += "\n"
+
+        exp_str += "N  1  {}".format(data.materials[0].name.split(" ")[-1])
+        bpy.ops.object.select_pattern(pattern=obj.name)
+        bpy.ops.object.delete(use_global=True)
+
+        # we know that the objects only consist of one material
+        file_name = mat_file_name_dict[data.materials[0].name]
+        with open(file_name, "a") as f:
+            f.write(exp_str)
+
+# print([x for x in bpy.context.scene.objects[0].data.polygons[:2]])
 # vertices_diel, faces_diel = idt_lower.diel_faces.prep_blender_data()
 #
 # vertices, faces = idt_lower.prep_blender_data()
